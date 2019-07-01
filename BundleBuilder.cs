8934b7d70e99f61a63070eeb3e557b38b5030c55
KurienEapen <kurieneapenk@gmail.com>
2019-07-01 22:24:08 +0530

    For making asset bundles, code must be inside Editor folder

---
+++ BundleBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class BundleBuilder : Editor {
+
+    [MenuItem("Assets/ Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+      
+        BuildPipeline.BuildAssetBundles(@"C:\Users\kurie\Desktop\AssetBundles\Android", BuildAssetBundleOptions.None, BuildTarget.Android);
+    }
+}
